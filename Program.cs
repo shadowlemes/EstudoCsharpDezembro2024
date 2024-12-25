df5c074614035170ca217943779d8d8ed632b3cf
Gabriel Lemes <Gabriel Lemes@DESKTOP-OIIUVQC>
2024-12-25 17:11:05 -0300

    Adicionar arquivos de projeto.

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine(DateTime.Now);
+        Perguntas();
+    }
+    static void Perguntas()
+    {
+        string nome;
+        int ano = 0;
+        int data = DateTime.Now.Year;       
+        Console.Write("Insira seu nome: ");
+        nome = Console.ReadLine()!;
+        Console.Clear();
+        Console.Write("Qual sua data de nascimento? ");
+        ano = int.Parse(Console.ReadLine()!);
+        int calculoIdade = data - ano;
+        Console.Clear();
+    }
+}
